13c2e550d766e7d763bd582837fe67597743008e
anait31 <anaitullah198@gmail.com>
2024-11-17 21:48:47 +0800

    Upload json data

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
                     <Link to={'/'}>LBingo</Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1 space-x-4 text-xl">
+                    <ul className="menu menu-horizontal px-1 space-x-4">
                         {links}
                     </ul>
                 </div>
